96eb4bf5cff7f0df644ad4245a2e2aa80765dfdd
Yaroslav <warlok38@mail.ru>
2022-08-18 14:33:19 +0700

    fix: fixed dependencies and added icon

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import cn from "classnames";
 import { ButtonProps } from "./Button.props";
 import styles from "./Button.module.css";
+import ArrowIcon from "./arrow.svg";
 
 export const Button = ({
   appearance,
+  arrow = "none",
   children,
   className,
   ...props
@@ -18,6 +20,11 @@ export const Button = ({
       {...props}
     >
       {children}
+      {arrow !== "none" && (
+        <span className={cn(styles.arrow, { [styles.down]: arrow === "down" })}>
+          <ArrowIcon />
+        </span>
+      )}
     </button>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,9 @@ export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag="h1">Ура</Htag>
-      <Button appearance="primary">Кнопка</Button>
+      <Button appearance="primary" arrow="right">
+        Кнопка
+      </Button>
     </div>
   );
 }
