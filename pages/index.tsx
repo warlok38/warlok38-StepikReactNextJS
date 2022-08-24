7e85561ff70e45bdb4fb65c3200b23ff9fb83b50
Yaroslav <warlok38@mail.ru>
2022-08-24 15:18:54 +0700

    feat: added Tag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { Htag } from "./../components/Htag/Htag";
 import { Button } from "./../components/Button/Button";
+import { P } from "../components";
+import { Tag } from "./../components/Tag/Tag";
 
 export default function Home(): JSX.Element {
   return (
@@ -8,6 +10,20 @@ export default function Home(): JSX.Element {
       <Button appearance="primary" arrow="right">
         Кнопка
       </Button>
+      <Button appearance="ghost" arrow="down">
+        Кнопка
+      </Button>
+      <P size="l">Большой</P>
+      <P>Средний</P>
+      <P size="s">Маленький</P>
+      <Tag size="s">ghost</Tag>
+      <Tag size="m" color="red">
+        red
+      </Tag>
+      <Tag size="s" color="green">
+        green
+      </Tag>
+      <Tag color="primary">primary</Tag>
     </div>
   );
 }
