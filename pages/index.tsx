994d374acdaecd0b34739b9777824f9c1624b3df
Yaroslav <warlok38@mail.ru>
2022-08-25 14:31:17 +0700

    feat: added axios and types

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,11 @@ import { Tag } from "./../components/Tag/Tag";
 import { Rating } from "./../components/Rating/Rating";
 import { useState } from "react";
 import { withLayout } from "../layout/Layout";
+import { GetStaticProps } from "next";
+import axios from "axios";
+import { MenuItem } from "../interfaces/menu.interface";
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
@@ -35,3 +38,25 @@ function Home(): JSX.Element {
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+    {
+      firstCategory,
+    }
+  );
+
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
